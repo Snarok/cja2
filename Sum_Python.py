219be4158c4ad77ceb6a31f703383a57178f6232
Stheven Obando <133877764+Snarok@users.noreply.github.com>
2023-05-24 07:56:07 -0500

    Create Sum_Python.py

---
+++ Sum_Python.py
@@ -0,0 +1,9 @@
+# Pedir al usuario que ingrese los números a sumar
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Sumar los números ingresados
+suma = numero1 + numero2
+
+# Imprimir el resultado
+print("La suma es:", suma)
